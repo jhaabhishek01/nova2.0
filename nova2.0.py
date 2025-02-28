76cff3e671164762a371a691edaef5577246de9e
Abhishek jha <abhishekkumarj665@gmail.com>
2025-02-28 17:17:21 +0530

    Update nova2.0.py

--- nova2.0.py
+++ nova2.0.py
@@ -4,9 +4,9 @@ $                                                     $
 $                     (N.O.V.A)                       $
 $                                                     $
 $           Version: 3.2.5                            $
-$           Started On: 2jan , 2025                   $
-$           Created By:  sakshara                     $
-$           Github ID:  saksharasrivastava            $
+$           Started On: 28nov , 2024                  $
+$           Created By:  Abhishek kumar jha           $
+$           Github ID:  jhaabhishek01                 $
 $                                                     $                                                                                                                                                                                            
 $                                                     $
 $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
@@ -65,10 +65,10 @@ test = toast()
 
 
 emails = {
-    "admin": "sakshara",
+    "admin": "Abhishek kumar jha",
 }
 
-city="sonebhadra" # for weather
+city="Darbhanga" # for weather
 
 months = {1:'January',2:'February',3:'March',4:'April',5:'May',6:'June',7:'July',8:'August',9:'September',10:'October',11:'November',12:'December'}
 dates = {1:'First',2:'Second',3:'Third',4:'Fourth',5:'Fifth',6:'Sixth',7:'Seventh',8:'Eighth',9:'Ninth',10:'Tenth',11:'Eleventh',12:'Twelfth',13:'Thirteenth',14:'Fourteenth',15:'Fifteenth',16:'Sixteenth',17:'Seventeenth',18:'Eighteenth',19:'Ninteenth',20:'Twentieth',21:'Twenty First',22:'Twenty Second',23:'Twenty Third',24:'Twenty Fourth',25:'Twenty Fifth',26:'Twenty Sixth',27:'Twenty Seventh',28:'Twenty Eighth',29:'Twenty Ninth',30:'Thirty',31:'Thirty First'}
